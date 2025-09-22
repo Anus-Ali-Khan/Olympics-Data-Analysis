3f3f26d4740a74ca35e17af1bc519e81b40c1b97
Anus-Ali-Khan <anuslaikhan1405@gmail.com>
2025-09-22 12:08:45 +0500

    feat: filters completed on basis of medal tally

---
+++ preprocessor.py
@@ -0,0 +1,14 @@
+import pandas as pd;
+
+
+
+def preprocess(df,df_region):
+    # filtering for summer olympics
+    df = df[df['Season'] == 'Summer']
+    # merge with region_df
+    df = df.merge(df_region,on='NOC', how='left')
+    # dropping duplicates
+    df.drop_duplicates(inplace=True)
+    # one hot encoding medals
+    df = pd.concat([df,pd.get_dummies(df['Medal'])],axis=1)
+    return df
